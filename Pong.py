fff6fcb02cd36ad0635469e1af755a01e160ce92
Doğukan Karakaş <37842289+Hanofflane@users.noreply.github.com>
2019-01-24 00:40:33 +0300

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -26,9 +26,72 @@ paddle_b.goto(350, 0)
 
 
 #Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 1
+ball.dy = 1
 
+#Functions
+def paddle_a_up():
+	y = paddle_a.ycor()
+	y += 20
+	paddle_a.sety(y)
+
+def paddle_a_down():
+	y = paddle_a.ycor()
+	y -= 20
+	paddle_a.sety(y)
+
+def paddle_b_up():
+	y = paddle_b.ycor()
+	y += 20
+	paddle_b.sety(y)
+
+def paddle_b_down():
+	y = paddle_b.ycor()
+	y -= 20
+	paddle_b.sety(y)
+
+
+#Bindings
+
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 
 #Main game loop
 while True:
 	wn.update()
+
+	#move the ball
+	ball.setx(ball.xcor() + ball.dx)
+	ball.sety(ball.ycor() + ball.dy)
+
+	#border checking
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
+
+	if ball.ycor() < -290:
+		ball.sety(-290)
+		ball.dy *= -1
+
+	if ball.xcor() > 390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+	if ball.xcor() < -390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+		#paddle and ball collision
+		if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
+				ball.setx(340)
+				ball.dx *= -1
